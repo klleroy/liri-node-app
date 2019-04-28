aef335527696c1bf57005b36f6e5d1868a99a4da
KevinL <mastahkevin@gmail.com>
2019-04-28 13:54:44 -0700

    Adding functionality to direct which function is run.

--- liri.js
+++ liri.js
@@ -6,5 +6,16 @@ spotify = new Spotify(_spotify);
 import moment from 'moment';
 import fs from 'fs';
 import request from 'request';
-
 import axios from 'axios';
+
+let action = process.argv[2];
+let value = process.argv[3];
+
+switch (action) {
+    case 'concert-this':
+        
+        break;
+
+    default:
+        break;
+}
