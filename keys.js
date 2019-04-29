0fe27aaa5c4e30cf6416d244d7a00c98955190b2
KevinL <mastahkevin@gmail.com>
2019-04-28 17:54:46 -0700

    Changing JS to use ES5 rather than ES6 which is not supported by Node as of yet.

--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
 console.log('this is loaded');
 
-export const spotify = {
-	id: process.env.SPOTIFY_ID,
-	secret: process.env.SPOTIFY_SECRET
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.spotify = void 0;
+var spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
+exports.spotify = spotify;
--- liri.js
+++ liri.js
@@ -1,24 +1,43 @@
 require('dotenv').config();
 
-import { spotify as _spotify } from './keys.js';
-import Spotify from 'node-spotify-api';
-spotify = new Spotify(_spotify);
-import moment from 'moment';
-import fs from 'fs';
-import request from 'request';
-import axios from 'axios';
+const keys = require('./keys.js');
+const spotify = new Spotify(keys.spotify);
+const moment = require('moment');
+const fs = require('fs');
+const request = require('request');
+const axios = require('axios');
 
 let action = process.argv[2];
 let value = process.argv[3];
 
 switch (action) {
-    case 'concert-this':
-        concertThis(value);
-        break;
+    // case 'concert-this':
+    //     concertThis(value);
+    //     break;
 
     case 'spotify-this-song':
         spotifyThisSong(value);
         break;
 }
 
-// funtions called
+function spotifyThisSong(value) {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+}
+
+// functions called
+function spotifyThisSong(value) {
+    spotify
+        .search({ type: 'track', query: 'All the Small Things' })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
