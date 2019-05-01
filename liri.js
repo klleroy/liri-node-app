d1a765665b4f35c7689c8297ac72576179218635
KevinL <mastahkevin@gmail.com>
2019-04-30 22:42:49 -0700

    Added axios.get import to movieThis function.

--- liri.js
+++ liri.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 const keys = require('./keys.js');
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const axios = require('axios');
 const moment = require('moment');
 const fs = require('fs');
 const request = require('request');
-const axios = require('axios');
 
 let action = process.argv[2];
 let value = process.argv[3];
@@ -25,15 +25,38 @@ switch (action) {
         break;
 
     case 'movie-this':
-        movieThis(value);
+        movieThis(value); ''
         break;
 
     case 'do-what-it-says':
         doWhatItSays(value);
 }
 
+/*
+  * Title of the movie.
+  * Year the movie came out.
+  * IMDB Rating of the movie.
+  * Rotten Tomatoes Rating of the movie.
+  * Country where the movie was produced.
+  * Language of the movie.
+  * Plot of the movie.
+  * Actors in the movie.
+*/
 
-// 
+function movieThis(value) {
+    // Then run a request with axios to the OMDB API with the movie specified
+    axios.get(
+        "http://omdbapi.com/?t=" + value + "&apikey=trilogy")
+        .then(
+            function (response) {
+            console.log(
+                '~~~~~~~~~~~~~~~~~\n' + "Title: " + response.data.Title
+            );
+        }
+    )
+}
+
+// function to spotify song. Takes value variable from switch case up top.
 function spotifyThisSong(value) {
     spotify.search({
         type: 'track',
@@ -43,9 +66,10 @@ function spotifyThisSong(value) {
             return console.log('Error occurred: ' + err);
         }
         else if (data) {
-            console.log('~~~~~~~~~~~~~~~~~');
-            console.log('Artist: ' + data.tracks.items[0].album.artists[0].name + '\nSong: ' + data.tracks.items[0].name + '\nURL: ' + 
-            data.tracks.items[0].external_urls.spotify + '\nAlbum: ' + data.tracks.items[0].album.name);
+            console.log('~~~~~~~~~~~~~~~~~\n');
+            console.log('Artist: ' + data.tracks.items[0].album.artists[0].name + '\nSong: ' + data.tracks.items[0].name + '\nURL: ' +
+                data.tracks.items[0].external_urls.spotify + '\nAlbum: ' + data.tracks.items[0].album.name);
         }
     });
 };
+
