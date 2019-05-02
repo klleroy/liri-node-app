19c84091e7595b6262ab71e7b646f59139af3260
KevinL <mastahkevin@gmail.com>
2019-05-01 22:31:24 -0700

    Refined functions to be more readable in liri. Removed console.log from keys. Cleared log.txt & removed  from random.txt which was causing errors.

--- liri.js
+++ liri.js
@@ -36,33 +36,29 @@ switch (action) {
 // concert search
 function concertThis(value) {
     axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
-    .then(
-        function (response) {
-            // loop through JSON Array to get concert info.
-            for (let i = 0; i < response.data.length; i++) {
-                let venue = response.data[i].venue.name;
-                logToFile("\nVenue Name: " + venue);
-                let country = response.data[i].venue.country;
-                logToFile("Country: " + country);
-                let city = response.data[i].venue.city;
-                logToFile("City: " + city);
-                let date = moment(response.data[i].datetime).format('MM/DD/YYYY');
-                logToFile("Date: " + date);             
+        .then(
+            function (response) {
+                // loop through JSON Array to get concert info.
+                for (let i = 0; i < response.data.length; i++) {
+                    logToFile("\nVenue Name: " + response.data[i].venue.name);
+                    logToFile("Country: " + response.data[i].venue.country);
+                    logToFile("City: " + response.data[i].venue.city);
+                    logToFile("Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'));
+                }
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    logToFile(error.response.data);
+                    logToFile(error.response.status);
+                    logToFile(error.response.headers);
+                } else if (error.request) {
+                    logToFile(error.request);
+                } else {
+                    logToFile("Error", error.message);
+                }
+                logToFile(error.config);
             }
-        },
-        function (error) {
-            if (error.response) {
-                logToFile(error.response.data);
-                logToFile(error.response.status);
-                logToFile(error.response.headers);
-            } else if (error.request) {
-                logToFile(error.request);
-            } else {
-                logToFile("Error",error.message);
-            }
-            logToFile(error.config);            
-        }
-    )
+        )
 }
 
 // function to spotify song. Takes value variable from switch case up top.
@@ -75,38 +71,12 @@ function spotifyThisSong(value) {
             return logToFile('Error occurred (Spotify): ' + err);
         }
         else if (data) {
-            logToFile("\n-------------------\nspotify-this-song\n" + "-------------------\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nSong: " + data.tracks.items[0].name + 
+            logToFile("\n-------------------\nspotify-this-song\n" + "-------------------\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nSong: " + data.tracks.items[0].name +
                 "\nURL: " + data.tracks.items[0].external_urls.spotify + "\nAlbum: " + data.tracks.items[0].album.name
             );
         }
     });
 };
-
-// Search OMDB API for movie and returns the following...
-/* 
-    - Title of the movie
-    - Year the movie came out
-    - IMDB Rating of the movie
-    - Rotten Tomatoes Rating of the movie
-    - Country where the movie was produced.
-    - Language of the movie.
-    - Plot of the movie.
-    - Actors in the movie.
-*/
-
-// function movieThis(value) {
-//     if (value == null) {
-//         value = "Mr. Nobody"
-//     }
-//     // Then run a request with axios to the OMDB API with the movie specified
-//     axios.get(
-//         "http://omdbapi.com/?t=" + value + "&apikey=trilogy")
-//         .then(
-//             function (response) {
-
-//             }
-//         )
-// };
 
 function movieThis(value) {
     if (value === null) {
@@ -115,20 +85,13 @@ function movieThis(value) {
     // originally concatenated these logs but decided to change for readability.
     axios.get('http://www.omdbapi.com/?apikey=trilogy&t=' + value)
         .then(function (response) {
-            let title = response.data.Title;
-            logToFile('\nTitle: ' + title);
-            let year = response.data.Year;
-            logToFile('Year: ' + year);
-            let imdbRating = response.data.imdbRating;
-            logToFile('IMDB Rating: ' + imdbRating);
-            let rottenToms = response.data.Ratings[1].Value;
-            logToFile('Rotten Tomatoes Rating: ' + rottenToms);
-            let country = response.data.Country;
-            logToFile('Country Produced In: ' + country);
-            let plot = response.data.Plot;
-            logToFile('Plot: ' + plot);
-            let actors = response.data.Actors;
-            logToFile('Actors: ' + actors);
+            logToFile('\nTitle: ' + response.data.Title);
+            logToFile('Year: ' + response.data.Year);
+            logToFile('IMDB Rating: ' + response.data.imdbRating);
+            logToFile('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
+            logToFile('Country Produced In: ' + response.data.Country);
+            logToFile('Plot: ' + response.data.Plot);
+            logToFile('Actors: ' + response.data.Actors);
         }),
         function (error) {
             if (error.response) {
@@ -138,14 +101,14 @@ function movieThis(value) {
             } else if (error.request) {
                 logToFile(error.request);
             } else {
-                logToFile("Error",error.message);
+                logToFile("Error", error.message);
             }
             logToFile(error.config);
         }
 }
 
 function doWhatItSays() {
-    fs.readFile("random.txt","utf8", function (err, data) {
+    fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
             return logToFile(err);
         }
@@ -161,11 +124,11 @@ function doWhatItSays() {
 }
 
 function appendToFile(text) {
-    fs.appendFile("log.txt", `${text}\n`, function (err) {
+    fs.appendFile("log.txt", `${text},\n`, function (err) {
 
         // If an error was experienced we will log it.
         if (err) {
-           return logToFile(err);
+            return logToFile(err);
         }
     });
 }
